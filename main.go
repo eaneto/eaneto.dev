d54ed1f485be6a7c77c2f491a688d7ae4c46b714
Edison Neto <edison.aguiar.neto@gmail.com>
2022-12-27 21:04:52 -0300

    Add internal in memory cache for the html templates

--- main.go
+++ main.go
@@ -9,19 +9,21 @@ import (
 	"github.com/eaneto/eanetodev/pagedata"
 )
 
-// TODO read file from memory
+var templates = make(map[string][]byte)
+
 func simpleGetHandler(writer http.ResponseWriter, request *http.Request, templateName string, title string) {
 	if request.Method == "GET" {
-		tmplt, err := template.ParseFiles("public/template/base.html")
+		content, err := readTemplateFromCacheOrFile("base")
 		if err != nil {
 			writer.WriteHeader(http.StatusInternalServerError)
 			return
 		}
-		content, err := ioutil.ReadFile(fmt.Sprintf("public/template/%s.html", templateName))
+		tmplt, err := template.New("base").Parse(string(content))
 		if err != nil {
 			writer.WriteHeader(http.StatusInternalServerError)
 			return
 		}
+		content, err = readTemplateFromCacheOrFile(templateName)
 		// sets response as cacheable for a week
 		writer.Header().Add("Cache-Control", "public,  max-age=604800")
 		data := pagedata.New(title, template.HTML(string(content)))
@@ -41,6 +43,19 @@ func handleArticles(writer http.ResponseWriter, request *http.Request) {
 
 func handleIndex(writer http.ResponseWriter, request *http.Request) {
 	simpleGetHandler(writer, request, "index", "Edison Aguiar")
+}
+
+func readTemplateFromCacheOrFile(templateName string) ([]byte, error) {
+	content, ok := templates[templateName]
+	if !ok {
+		content, err := ioutil.ReadFile(fmt.Sprintf("public/template/%s.html", templateName))
+		if err != nil {
+			return []byte{}, err
+		}
+		templates[templateName] = content
+		return content, nil
+	}
+	return content, nil
 }
 
 func main() {
